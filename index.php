a0c2a8654321dfac3a405bf28f83713fc2737391
nipoto <nipoto@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-28 20:08:35 +0000

    v0.1 hamyarwp
    git-svn-id: https://plugins.svn.wordpress.org/hamyarwp/tags/0.1@1169629 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+	/*
+		Plugin Name: hamyarWP
+		Plugin URI: https://wordpress.org/plugins/hamyarWP/
+		Description: ابزارک نمایش جدیدترین ارسال‌های همیار وردپرس ...
+		Version: 0.1
+		Author: Nima Saberi
+		Author URI: http://ideyeno.ir/
+		
+	*/
+	
+	function hamyarwp_feed()
+	{
+		require_once (ABSPATH . WPINC . '/rss.php');
+		$rss = @fetch_rss('http://hamyarwp.com/feed/');
+		if ( isset($rss->items) && count($rss->items) != 0 )
+		{
+			$rss->items = array_slice($rss->items, 0, 10);
+			echo "<ul>";
+			foreach ($rss->items as $item )
+			{
+				echo "<li><a href='".wp_filter_kses($item['link'])."' target='_blank'>".wp_specialchars($item['title'])."</a></li>";
+			}
+			echo "</ul>";
+		}
+	}
+	
+	function hamyarwp_widget()
+	{
+		wp_add_dashboard_widget('hamyarwp-widget', 'آخرین اخبار همیار وردپرس', 'hamyarwp_feed');
+	}
+	
+	add_action('wp_dashboard_setup', 'hamyarwp_feed');
+
+?>
